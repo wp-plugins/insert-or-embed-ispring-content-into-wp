ba37314ffa4c54ae47f8f3c99676b40e502ac58a
elearningplugins <elearningplugins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-07 17:10:48 +0000

    update functions.php
    git-svn-id: https://plugins.svn.wordpress.org/insert-or-embed-ispring-content-into-wp/tags/1.0@1084546 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- functions.php
+++ functions.php
@@ -769,7 +769,7 @@ else
 </table>
 </form>
 
-<p><i>Please choose a .zip file that you published with the iSpring software | <a href="http://www.elearningplugins.com/increase-maximum-upload-file-size/">Maximum upload file size</a>: <strong><?php echo $upload_size_unit; echo $sizes[$u]; ?></strong></i></p>
+<p><i>Please choose a .zip file that you published with the iSpring software | <a href="http://www.elearningplugins.com/increase-maximum-upload-file-size/" target="_blank">Maximum upload file size</a>: <strong><?php echo $upload_size_unit; echo $sizes[$u]; ?></strong></i></p>
 <img id="media_loading" style='display:none;' src= "<?php echo ispring_getPluginUrl() . "loading.gif" ;?>" /><br />
 <?php 
  	ispring_print_detail_form(1);
